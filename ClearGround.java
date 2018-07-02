40cef668cf3e42876dd24f92af93b110b524cc44
zhubochuan <40704749+zhubochuan@users.noreply.github.com>
2018-07-02 17:39:38 +1000

    Add files via upload

---
+++ ClearGround.java
@@ -0,0 +1,8 @@
+package Game;
+
+//This class stands for clear ground
+public class ClearGround extends GameItem{
+	ClearGround(char c) {
+		super(c); //invoke parent constructor from GameItem
+	}
+}
